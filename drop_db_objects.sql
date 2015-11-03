62ab52671ef54691feef4ac9932e8ec25463908c
cruepprich <cruepprich@gmail.com>
2015-11-03 15:24:44 -0600

    refactored cloudmgr.sql

---
+++ drop_db_objects.sql
@@ -0,0 +1,30 @@
+                                         
+
+drop PACKAGE DS_MGMT;                                                           
+drop PACKAGE DS_VIEWS;                
+drop VIEW DS_ACTIVE_DATABASES_V;                                                
+drop VIEW DS_ACTIVE_DB_COUNT_V;                                                 
+drop VIEW DS_DB_PLATFORM_IDS_V;                                                 
+drop VIEW DS_DB_PLATFORM_TEMPLATES_V;                                           
+drop VIEW DS_DB_STATS_V;                                                        
+drop VIEW DS_LOG_V;                                                             
+drop VIEW DS_SERVICE_REQUESTS_V;                                                
+drop VIEW DS_SERVICE_TEMPLATES_V;                                               
+drop VIEW DS_ZONES_V;                                                           
+drop VIEW DS_ACTIVE_DATABASES_V;                                                 
+drop TABLE DS_SAMPLE_CLOBS;                                                       
+drop type T_ACTIVE_DB_REC;                                                      
+drop type T_ACTIVE_DB_TAB;                                                      
+drop type T_DB_STATS_REC;                                                       
+drop type T_DB_STATS_TAB;                                                       
+drop type T_NAME_VALUE_REC;                                                     
+drop type T_NAME_VALUE_TAB;                                                     
+drop type T_SERVICE_REQUESTS_REC;                                               
+drop type T_SERVICE_REQUESTS_TAB;                                               
+drop type T_SERVICE_TEMPLATES_REC;                                              
+drop type T_SERVICE_TEMPLATES_TAB;                                              
+drop type T_VARCHAR2_REC;                                                       
+drop type T_VARCHAR2_TAB;                                                       
+drop type T_ZONES_REC;                                                          
+drop type T_ZONES_TAB;                                                          
+
